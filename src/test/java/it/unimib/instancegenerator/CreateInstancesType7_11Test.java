5e19ec829344566c2461383629e3bab37cb7aa2d
miciav <michele.ciavotta@gmail.com>
2020-07-04 19:23:01 +0200

    Instances of type 15 added. 100 with 5-10-15-20 families (type 20)

--- src/test/java/it/unimib/instancegenerator/CreateInstancesType7_11Test.java
+++ src/test/java/it/unimib/instancegenerator/CreateInstancesType7_11Test.java
@@ -200,22 +200,50 @@ public class CreateInstancesType7_11Test {
 //        assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
 //    }
 
-    @Test
-    public void instancesOfTypeBig19_075_085() {
-        // queste istanze sono quelle di grandi dimensioni
-        // 1) 20 zaini e 100 items, dim 2 e circa 10 famiglie
+//    @Test
+//    public void instancesOfTypeBig19_075_085() {
+//        // queste istanze sono quelle di grandi dimensioni
+//        // 1) 20 zaini e 100 items, dim 2 e circa 10 famiglie
+//
+//        utils6.setMinAlpha(0.75);
+//        utils6.setMaxAlpha(0.85);
+//        utils6.getProperties().getNumItems().setMaximum(13);
+//        utils6.getProperties().getNumItems().setMinimum(7);
+//        utils6.getProperties().setArrayTotalNumItems(new int[]{100});
+//        utils6.getProperties().setArrayNumKnapsacks(new int[]{20});
+//        utils6.getProperties().setArrayNunDimensions(new int[]{2});
+//        utils6.getProperties().setTypeName("type19");
+//        assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
+//    }
 
-        utils6.setMinAlpha(0.75);
-        utils6.setMaxAlpha(0.85);
+
+    @Test
+    public void instancesOfTypeBig20_10_045_055() {
+        // queste istanze sono quelle di grandi dimensioni
+        // 1) 10 zaini e 100 items, dim 2 e circa 5-10-15-20 famiglie
+
+        utils6.setMinAlpha(0.45);
+        utils6.setMaxAlpha(0.55);
+        utils6.getProperties().setArrayTotalNumItems(new int[]{100});
+        utils6.getProperties().setArrayNumKnapsacks(new int[]{10});
+        utils6.getProperties().setArrayNunDimensions(new int[]{2});
+
+        utils6.getProperties().getNumItems().setMaximum(23);
+        utils6.getProperties().getNumItems().setMinimum(17);
+        utils6.getProperties().setTypeName("type20-5");
+        assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
         utils6.getProperties().getNumItems().setMaximum(13);
         utils6.getProperties().getNumItems().setMinimum(7);
-        utils6.getProperties().setArrayTotalNumItems(new int[]{100});
-        utils6.getProperties().setArrayNumKnapsacks(new int[]{20});
-        utils6.getProperties().setArrayNunDimensions(new int[]{2});
-        utils6.getProperties().setTypeName("type19");
+        utils6.getProperties().setTypeName("type20-10");
+        assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
+        utils6.getProperties().getNumItems().setMaximum(8);
+        utils6.getProperties().getNumItems().setMinimum(6);
+        utils6.getProperties().setTypeName("type20-15");
+        assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
+        utils6.getProperties().getNumItems().setMaximum(6);
+        utils6.getProperties().getNumItems().setMinimum(4);
+        utils6.getProperties().setTypeName("type20-20");
         assertThat(shell.evaluate(() -> "create-instances-type-g")).isEqualTo("Instances generated !!");
     }
-
-
 
 }
